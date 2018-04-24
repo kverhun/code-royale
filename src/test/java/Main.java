18fad418c28d6070c3f5b14a8c42d8555cb03e99
Kostiantyn Verhun <Kostiantyn.Verhun@ring.com>
2018-04-24 10:15:04 +0300

    get bots from cmd

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -6,11 +6,21 @@ public class Main {
 
         GameRunner gameRunner = new GameRunner();
 
+        if (args.length < 2)
+            return;
+
+        for (int i = 1; i < args.length; ++i)
+            System.out.println(args[i]);
+
+
         // Adds as many player as you need to test your game
-        gameRunner.addAgent(Level1Player.class);
-        gameRunner.addAgent(ThibaudPlayer.class);
+        //gameRunner.addAgent(Level1Player.class);
+        //gameRunner.addAgent(ThibaudPlayer.class);
 
         // gameRunner.addCommandLinePlayer("python3 /home/user/player.py");
+        gameRunner.addCommandLinePlayer(args[0]);
+        gameRunner.addCommandLinePlayer(args[1]);
+        //gameRunner.addAgent(Level1Player.class);
 
         gameRunner.start();
     }
